85440e65319a314515f33f8cc15c5a810c09190c
Slim Amamou <slim@gouza7.(none)>
2008-02-12 19:43:09 +0100

    coloration syntaxique dans l'historique

--- bazdig/history/index.php
+++ bazdig/history/index.php
@@ -17,6 +17,7 @@
 ?>
 <title>bazdig history</title>
 <link rel="stylesheet" type="text/css" href="../bazdig.css" />
+<link rel="stylesheet" type="text/css" href="../codepress/languages/sql.css" />
 <div id="nav"><a href="../console/" accesskey="c" title="(c)" class="button">console</a><a href="../bazdig.db" accesskey="s" title="(s)" class="button">save</a></div>
 <div id="history">
 <form method="get" action=".">
@@ -25,7 +26,7 @@
 <div id="queries">
 <?php
 	foreach ($queries as $q) {
-		echo '<a href="'. $console->get_url() .'?q='. rawurlencode($q->code) .'" ><pre>'. $q->code .'</pre></a>';
+		echo '<pre onclick="document.location.href=\''. $console->get_url() .'?q='. rawurlencode($q->code) .'\'" >'. $q->toHTML().'</pre>';
 	}
 ?>
 </div>
