4c893199f6e41118a59dfe213da4d640c4ca82a0
Slim Amamou <slim.amamou@gmail.com>
2009-09-03 22:31:46 +0100

    bugfix: access using /bazdig/index.php was redirecting infinitely

--- bazdig/index.php
+++ bazdig/index.php
@@ -1,3 +1,7 @@
 <?php
+	require "../lib/localresource.php";
 	$url  = "http://". $_SERVER['SERVER_NAME'] ."/". $_SERVER['REQUEST_URI'];
-	header("Location: $url/console/");
+	$file = __FILE__;
+	$here = new LocalResource($url, $file);
+	$console = $here->base()->get("/console/")->url;
+	header("Location: $console", true, 307);
